0514c0f4fc4d6991604e00a3ff1861ad20c1148d
T3rm1 <T3rm1@outlook.com>
2016-12-08 10:49:07 +0100

    Log a warning if a session attribute is not serializable

--- src/main/java/com/hazelcast/web/HazelcastHttpSession.java
+++ src/main/java/com/hazelcast/web/HazelcastHttpSession.java
@@ -16,6 +16,7 @@
 
 package com.hazelcast.web;
 
+import com.hazelcast.nio.serialization.HazelcastSerializationException;
 import com.hazelcast.util.EmptyStatement;
 
 import javax.servlet.ServletContext;
@@ -105,6 +106,8 @@ public class HazelcastHttpSession implements HttpSession {
             try {
                 webFilter.getClusteredSessionService().setAttribute(id, name, value);
                 entry.setDirty(false);
+            } catch (HazelcastSerializationException hse) {
+                WebFilter.LOGGER.warning(hse.getMessage());
             } catch (Exception ignored) {
                 EmptyStatement.ignore(ignored);
             }

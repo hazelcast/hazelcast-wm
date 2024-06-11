e78dab734fe103aec18844eee406f4af0741360d
Orçun Çolak <orcun.colak@hazelcast.com>
2024-06-11 11:44:10 +0300

    Upgrade to jakarta namespace

--- src/test/java/com/hazelcast/wm/test/spring/TestController.java
+++ src/test/java/com/hazelcast/wm/test/spring/TestController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import javax.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletRequest;
 
 @SuppressWarnings("unused")
 @Controller
